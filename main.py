7d431181c1e2683ef962a5c055170180fc25219f
lorhenha279816 <lorhenha279816@gmail.com>
2025-07-31 21:59:16 -0400

    Nueva linea

--- main.py
+++ main.py
@@ -14,12 +14,16 @@ class Numero:
                 print("\nEl numero ingresado es 10")
     def sumar(self,numeroasumar):
         return self.numero + numeroasumar
-    
-    
-    
+      
+
+
+
 if __name__ == "__main__":
     
     numeroaevaluar= Numero(int(input("Ingrese un numero: \n")))
     numeroaevaluar.evaluarNumero()
     sumarealizada =numeroaevaluar.sumar(2)
-    print("\nLa suma realizada es: ",sumarealizada)
+    print("\nLa suma realizada es: ",sumarealizada) 
+    print("Nueva Linea")
+    
+    
